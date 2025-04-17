c64802bc7079c33a409420d7c88ca4d49a6f6c65
avizith <mithunavizithdash2222@gmail.com>
2025-04-17 20:48:14 +0600

    Update phy 2-1.c

--- phy 2-1.c
+++ phy 2-1.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+#include <math.h>
 
-void findOrder(double lambda, double d) {
-
+void findOrder(double lambda, double d, double theta) {
     if (lambda < 380 || lambda > 750) {
         printf("Out of the range. Please enter a valid number.\n");
         return;
     }
-    d= d * 1000;
 
+    lambda = lambda * 1e-9;
+    d = d * 1e-6;
+    double theta_rad = theta * M_PI / 180.0;
 
-    int m = (int)(d / lambda);
-
+    double m_calc = (d * sin(theta_rad)) / lambda;
+    int m = (int)(round(m_calc));
 
     if (m > 0) {
         printf("%d-th order maxima\n", m);
@@ -20,14 +22,15 @@ void findOrder(double lambda, double d) {
 }
 
 int main() {
-    double lambda, d;
-
+    double lambda, d, theta;
 
     printf("Enter wavelength (in nm): ");
     scanf("%lf", &lambda);
     printf("Enter slit separation (in micrometers): ");
     scanf("%lf", &d);
+    printf("Enter angle theta (in degrees): ");
+    scanf("%lf", &theta);
 
-    findOrder(lambda, d);
+    findOrder(lambda, d, theta);
     return 0;
 }
